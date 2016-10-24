2cec1102fedda46c8dfba4eb1b1ecb98f087f3d0
AnishS <anish.spati@gmail.com>
2016-10-23 20:19:18 -0400

    Finished Rest of MultiVariable Regression
    Completed multilinear regression and normal equation. 100% on
    assignment now.

--- Assignment 1/ex1_multi.m
+++ Assignment 1/ex1_multi.m
@@ -54,7 +54,6 @@ fprintf('Normalizing Features ...\n');
 % Add intercept term to X
 X = [ones(m, 1) X];
 
-
 %% ================ Part 2: Gradient Descent ================
 
 % ====================== YOUR CODE HERE ======================
@@ -89,9 +88,26 @@ num_iters = 400;
 theta = zeros(3, 1);
 [theta, J_history] = gradientDescentMulti(X, y, theta, alpha, num_iters);
 
+alpha1 = .03;
+theta1 = zeros(3,1);
+[theta1, J_history1] = gradientDescentMulti(X,y,theta1,alpha1,num_iters);
+
+alpha2 = .1;
+theta2 = zeros(3,1);
+[theta2, J_history2] = gradientDescentMulti(X,y,theta2,alpha2,num_iters);
+
+alpha3 = 1;
+theta3 = zeros(3,1);
+[theta3, J_history3] = gradientDescentMulti(X,y,theta3,alpha3,num_iters);
 % Plot the convergence graph
 figure;
 plot(1:numel(J_history), J_history, '-b', 'LineWidth', 2);
+hold on;
+plot(1:numel(J_history1), J_history1, '-r', 'LineWidth', 2);
+hold on;
+plot(1:numel(J_history2), J_history2, '-g', 'LineWidth', 2);
+hold on;
+plot(1:numel(J_history3), J_history3, '-y', 'LineWidth', 2);
 xlabel('Number of iterations');
 ylabel('Cost J');
 
@@ -104,7 +120,7 @@ fprintf('\n');
 % ====================== YOUR CODE HERE ======================
 % Recall that the first column of X is all-ones. Thus, it does
 % not need to be normalized.
-price = 0; % You should change this
+price = [1, (1650-mu(1))/sigma(1), (3-mu(2))/sigma(2)] * theta2; % You should change this
 
 
 % ============================================================
@@ -149,7 +165,7 @@ fprintf('\n');
 
 % Estimate the price of a 1650 sq-ft, 3 br house
 % ====================== YOUR CODE HERE ======================
-price = 0; % You should change this
+price = [1,1650,3]*theta; % You should change this
 
 
 % ============================================================
